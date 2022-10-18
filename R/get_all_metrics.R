a24104d6e87a305c43a458fd2a6f50919fd624aa
jbryer <jason@bryer.org>
2022-10-18 19:51:07 -0400

    Added comments for possible refactor to include metric type in returned object.

--- R/get_all_metrics.R
+++ R/get_all_metrics.R
@@ -8,11 +8,15 @@
 #' @export
 #' @return a list with all the metric functions from the `yardstick` package.
 #' @import yardstick
-get_all_metrics <- function() {
+get_all_metrics <- function(
+		# include_classification = TRUE,
+		# include_regression = TRUE,
+		include_multiclass = FALSE
+) {
 	metrics <- list()
 	library(yardstick)
 	pos <- which(search() == 'package:yardstick')
-	rd_yardstick <- tools::Rd_db('yardstick')
+	# rd_yardstick <- tools::Rd_db('yardstick')
 	for(i in ls('package:yardstick')) {
 		obj <- get(i, pos = pos)
 		if(is.function(obj) & 'metric' %in% class(obj)) {
@@ -21,6 +25,18 @@ get_all_metrics <- function() {
 			metrics[[i]] <- obj
 		}
 	}
-	metrics <- metrics[!names(metrics) %in% c('roc_aunp', 'roc_aunu')] # Excluding multiclass metrics
+	# metric_types <- sapply(metrics, FUN = function(x) { class(x)[1] })
+	# numeric_metrics <- metrics[metric_types == 'numeric_metric']
+	# class_metrics <- metrics[metric_types == 'class_metric']
+	# class_probability_metrics <- metrics[metric_types == 'prob_metric']
+	# if(!include_classification) {
+	#
+	# }
+	# if(!include_regression) {
+	#
+	# }
+	if(!include_multiclass) {
+		metrics <- metrics[!names(metrics) %in% c('roc_aunp', 'roc_aunu')] # Excluding multiclass metrics
+	}
 	invisible(metrics)
 }
